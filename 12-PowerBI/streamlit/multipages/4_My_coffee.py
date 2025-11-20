28326d4c24bc1f49592e01d64be621fc6b9ab759
ผศ.ดร. เอกรัฐ รัฐกาญจน์ <69342162+ekaratnida@users.noreply.github.com>
2025-11-20 14:30:21 +0700

    Add files via upload

--- 12-PowerBI/streamlit/multipages/4_My_coffee.py
+++ 12-PowerBI/streamlit/multipages/4_My_coffee.py
@@ -19,7 +19,32 @@ st.write(df1)
 selectedCity = st.sidebar.selectbox('Choose your city',['Bangkok','Chiangmai','Phuket'])
 results = db.sql(f"SELECT \"Store Name\" FROM df1 where City='{selectedCity}'").df()
 #results2 = db.sql(f"select \"Store Name\" from results").df()
-st.write(results)
+
+if st.toggle("select"):
+
+    selection = st.dataframe(
+        results,
+        on_select="rerun",
+        selection_mode="single-cell", # Use single-cell mode
+        use_container_width=True
+    )
+
+    # Check if a cell has been selected
+    if selection and selection["selection"]["cells"]:
+        # The selection data is a dictionary.
+        # selection["selection"]["cells"] contains a list of (row, col) tuples
+        for row_index, col_name in selection["selection"]["cells"]:
+            # Get the value of the selected cell
+            cell_value = results.iloc[row_index][col_name]
+            
+            # Trigger other components or logic
+            st.subheader("Details for the selected cell:")
+            st.write(f"Row Index: {row_index}, Column: {col_name}, Value: {cell_value}")        
+    else:
+        st.info("Select a cell to see the details and update the chart.")
+
+else:
+    st.write(results)
 
 st.markdown(
     """
